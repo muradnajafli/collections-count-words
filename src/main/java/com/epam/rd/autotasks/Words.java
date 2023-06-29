61bd743f6d285ce452877b46c16206d06df01e96
Murad Najafli <muradnecefli05@gmail.com>
2023-06-29 11:10:09 +0400

    WordsCount

--- src/main/java/com/epam/rd/autotasks/Words.java
+++ src/main/java/com/epam/rd/autotasks/Words.java
@@ -1,11 +1,39 @@
 package com.epam.rd.autotasks;
 
 
-import java.util.List;
+import java.util.*;
 
 public class Words {
-
     public String countWords(List<String> lines) {
-        throw new UnsupportedOperationException();
+        HashMap<String, Integer> wordCountMap = new HashMap<>();
+
+        for (String line : lines) {
+            String[] words = line.split("[ .,‘’(“—/:?!”;*)'\"-]|\\s+]");
+
+            for (String word : words) {
+                String cleanedWord = word.toLowerCase();
+                if (cleanedWord.length() >= 4) {
+                    wordCountMap.put(cleanedWord, wordCountMap.getOrDefault(cleanedWord, 0) + 1);
+                }
+            }
+        }
+
+        Iterator<Map.Entry<String, Integer>> iterator = wordCountMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, Integer> entry = iterator.next();
+            if (entry.getValue() < 10) {
+                iterator.remove();
+            }
+        }
+
+        List<Map.Entry<String, Integer>> sortedEntries = new ArrayList<>(wordCountMap.entrySet());
+        sortedEntries.sort(Map.Entry.<String, Integer>comparingByValue().reversed().thenComparing(Map.Entry.comparingByKey()));
+
+        StringBuilder result = new StringBuilder();
+        for (Map.Entry<String, Integer> entry : sortedEntries) {
+            result.append(entry.getKey()).append(" - ").append(entry.getValue()).append("\n");
+        }
+
+        return result.toString().trim();
     }
 }
